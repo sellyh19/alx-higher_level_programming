3fc110236b57d1856edb5666b460e3aa81ab0259
Selma Shikone <sellyh19@gmail.com>
2022-12-09 06:13:38 -0800

    tasks

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    return (list(map((lambda i: i * number), my_list)))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+
